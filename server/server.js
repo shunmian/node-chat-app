301f4f8fa0dd0facd1336dc36fadd0e99d1ce45d
shunmian <shunmian@gmail.com>
2018-08-02 09:03:30 +0800

    use process.env.PORT since heroku only allow dynamic port

--- server/server.js
+++ server/server.js
@@ -1,12 +1,13 @@
 const express = require('express')
 const path = require('path')
 
+const port = process.env.PORT || 3000
 const app = express()
 const viewRootPath = path.join(__dirname, './../public')
 
 app.use(express.static(viewRootPath))
 
-const server = app.listen(3000, () => {
+const server = app.listen(port, () => {
   const address = server.address()
   console.log(`server start on ${address.address}: ${address.port}`)
 })
